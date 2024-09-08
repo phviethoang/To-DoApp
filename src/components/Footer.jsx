a771e3df4b7a8196c3019e8d5f5c4b1333671fda
phviethoang <165120748+phviethoang@users.noreply.github.com>
2024-09-08 17:05:20 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year} by PVH to TLP with ❤ </p>
     </footer>
   );
 }
